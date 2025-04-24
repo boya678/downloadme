6f518eaa6026cdc56d6e68091c3b0b295b43a9d4
Your Name <you@example.com>
2025-04-23 20:31:51 -0500

    1.1

--- aplicacion/index.js
+++ aplicacion/index.js
@@ -4,15 +4,23 @@ const port = 3000;
 
 const OBJECT_COUNT = parseInt(process.env.OBJECT_COUNT) || 250000;
 
+app.get('/hostname', (req, res) => {
+  var data={
+    hostame: process.env.HOSTNAME,
+  }
+  res.send(data);
+});
+
 // Endpoint ligero
 app.get('/light', (req, res) => {
   const bigArray = [];
-
   for (let i = 0; i < OBJECT_COUNT/2; i++) {
     bigArray.push({ index: i, data: 'x'.repeat(100) }); // ~100 bytes en string + overhead
   }
-  console.log(`Array generado con ${OBJECT_COUNT/4} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`)
-  res.send(`Array generado con ${OBJECT_COUNT/4} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`);
+  var data={
+    array: bigArray
+  }
+  res.send(data);
 });
 
 // Endpoint con consumo alto de memoria
@@ -22,8 +30,10 @@ app.get('/heavy', (req, res) => {
   for (let i = 0; i < OBJECT_COUNT; i++) {
     bigArray.push({ index: i, data: 'x'.repeat(100) }); // ~100 bytes en string + overhead
   }
-  console.log(`Array generado con ${OBJECT_COUNT} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`)
-  res.send(`Array generado con ${OBJECT_COUNT} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`);
+  var data={
+    array: bigArray
+  }
+  res.send(data);
 });
 
 // Endpoint con consumo alto de memoria
@@ -33,8 +43,10 @@ app.get('/mid', (req, res) => {
   for (let i = 0; i < OBJECT_COUNT/2; i++) {
     bigArray.push({ index: i, data: 'x'.repeat(100) }); // ~100 bytes en string + overhead
   }
-  console.log(`Array generado con ${OBJECT_COUNT/2} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`)
-  res.send(`Array generado con ${OBJECT_COUNT/2} elementos (~128MB en memoria). host: ${process.env.HOSTNAME}`);
+  var data={
+    array: bigArray
+  }
+  res.send(data);
 });
 
 app.listen(port, () => {
